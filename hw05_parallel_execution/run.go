a14deb9349912c0a3df09fecf82adef8a07f961d
Sergey Zaslon <sergeyzaslon@gmail.com>
2021-11-30 07:57:38 +0200

    Hw05 parallel execution (#5)
    * HW4 is complete
    
    * HW05 is done
    
    * fix(lint): fixed linter warning
    
    * fix(concurrency): fixed concurrency read

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,8 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +12,52 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	var errCount uint64
+	var wg sync.WaitGroup
+	ch := make(chan Task)
+	defer close(ch)
+	quitCh := make(chan bool)
+	defer close(quitCh)
+
+	tasksLen := len(tasks)
+
+	// consumers
+	for i := 0; i < n && i < tasksLen; i++ {
+		wg.Add(1)
+		go func(ch chan Task, quitCh chan bool) {
+			for {
+				select {
+				case task := <-ch:
+					{
+						if task() != nil {
+							atomic.AddUint64(&errCount, 1)
+						}
+					}
+				case <-quitCh:
+					wg.Done()
+					return
+				}
+			}
+		}(ch, quitCh)
+	}
+
+	// producer
+	for _, task := range tasks {
+		if atomic.LoadUint64(&errCount) < uint64(m) {
+			ch <- task
+		} else {
+			break
+		}
+	}
+
+	for i := 0; i < n && i < tasksLen; i++ {
+		quitCh <- true
+	}
+
+	wg.Wait()
+
+	if m > 0 && atomic.LoadUint64(&errCount) >= uint64(m) {
+		return ErrErrorsLimitExceeded
+	}
 	return nil
 }
