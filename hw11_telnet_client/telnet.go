1129f7d061e3a9d97c6a509e02d8f1d59916a6dc
sergeyzaslon <sergeyzaslon@gmail.com>
2022-01-18 23:15:18 +0200

    HW11

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
+
+var ErrConnectionNotEstablished = errors.New("connection not established")
 
 type TelnetClient interface {
 	Connect() error
@@ -12,10 +17,57 @@ type TelnetClient interface {
 	Receive() error
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+type Telnet struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+}
+
+func (t *Telnet) Close() error {
+	t.in.Close()
+	if t.conn == nil {
+		return nil
+	}
+	return t.conn.Close()
+}
+
+func (t *Telnet) Connect() error {
+	conn, err := net.DialTimeout("tcp", t.address, t.timeout)
+	if err != nil {
+		return fmt.Errorf("unble to connect: %w", err)
+	}
+	t.conn = conn
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (t *Telnet) Send() error {
+	if t.conn == nil {
+		return ErrConnectionNotEstablished
+	}
+	return copyBytes(t.in, t.conn)
+}
+
+func (t *Telnet) Receive() error {
+	if t.conn == nil {
+		return ErrConnectionNotEstablished
+	}
+	return copyBytes(t.conn, t.out)
+}
+
+func copyBytes(src io.Reader, dest io.Writer) error {
+	if _, err := io.Copy(dest, src); err != nil {
+		return fmt.Errorf("unable to copyBytes: %w", err)
+	}
+	return nil
+}
+
+func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
+	return &Telnet{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
